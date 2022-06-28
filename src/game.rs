85af2034aff8dedc40199625352af827234ba0f0
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-06-27 20:40:30 -0700

    Tighten bot memory limits

--- src/game.rs
+++ src/game.rs
@@ -8,10 +8,10 @@ use std::thread;
 use std::time::Duration;
 use wasmtime::{Config, Engine, Module};
 
-pub const NETWORK_MEMORY_SIZE: usize = 1_000_000; // 1mb
+pub const NETWORK_MEMORY_SIZE: usize = 512_000; // 512kb
 pub const BOT_TIME_LIMIT: u64 = 100; // ~1ms, depending on scheduler behavior and when increment_epoch() is called
 pub const BOT_SETUP_TIME_LIMIT: u64 = 25; // ~0.25ms, depending on scheduler behavior and when increment_epoch() is called
-pub const BOT_MEMORY_LIMIT: usize = 4_000_000; // 4mb
+pub const BOT_MEMORY_LIMIT: usize = 2_000_000; // 2mb
 
 pub struct Game {
     players: Arc<DashMap<u64, Player>>,
